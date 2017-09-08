76b8925004403adbe5398694863eb8a37e88d141
ghost1372 <mahdidvb72@gmail.com>
2017-09-08 14:25:58 +0430

    First Commit

--- Contacts/Properties/AssemblyInfo.cs
+++ Contacts/Properties/AssemblyInfo.cs
@@ -37,8 +37,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.9.25.0")]
-[assembly: AssemblyFileVersion("0.9.18.0")]
+[assembly: AssemblyVersion("0.9.26.0")]
+[assembly: AssemblyFileVersion("0.9.19.0")]
 
 // TODO: Add these back before releasing.  Right now it's unnecessary overhead.
 [assembly:
